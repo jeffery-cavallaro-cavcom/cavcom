f4d12b650b07789c1b9cb32acfdd839fd3df6dea
Jeffery Cavallaro <jeffery@cavcom.com>
2019-11-07 11:28:23 -0800

    Finished TikZ formatter

--- graph/libgraphutil/tikz_formatter.cc
+++ graph/libgraphutil/tikz_formatter.cc
@@ -2,11 +2,11 @@
 
 namespace cavcom {
   namespace graph {
-    const std::string TikzFormatter::LABELED_STYLE = "labeled node/.style={draw,circle,minimum size=2em}";
+    const std::string TikzFormatter::LABELED_STYLE = "every node/.style={draw,circle,minimum size=2em}";
 
     const std::string TikzFormatter::UNLABELED_STYLE =
-      "unlabeled node/.style={draw,circle,fill=#1,inner sep=0cm,minimum size=0.2cm}"
-      ", unlabeled node/.default={black}";
+      "every node/.style={draw,circle,fill=#1,inner sep=0cm,minimum size=0.2cm}"
+      ", every node/.default={black}";
 
     TikzFormatter::TikzFormatter(std::ostream *out) : Formatter<std::string>(out) {}
 
@@ -16,34 +16,58 @@ namespace cavcom {
     }
 
     bool TikzFormatter::start_vertices(const Graph &graph) {
-      *out_ << "  \\begin{scope}[" << (vertex_labels() ? LABELED_STYLE : UNLABELED_STYLE) << "]" << std::endl;
+      *out_ << "\\begin{scope}[" << (vertex_labels() ? LABELED_STYLE : UNLABELED_STYLE) << "]" << std::endl;
       return out_->good();
     }
 
     bool TikzFormatter::format_vertex(const Vertex &vertex) {
-      //      const std::string &color = get_color(vertex.color());
-      //      *out_ << "  \\node";
-      //      if (!color.empty()) *out_ << " [fill=" << color << "]";
-      //      *out_ << " (" << vertex.id() << ") at ("
-      //            << vertex.xpos() << "," << vertex.ypos() << ") {"
-      //            << vertex.label() << "};"
-      //            << std::endl;
+      *out_ << "\\node";
+
+      if (vertex_colors()) {
+        const ColorType &color = get_color(vertex.color());
+        if (!color.empty()) *out_ << " [fill=" << color << "]";
+      }
+
+      *out_ << " (" << vertex.id() << ") at (" << vertex.xpos() << "," << vertex.ypos() << ") {";
+
+      if (vertex_labels()) {
+        const Label &label = vertex.label();
+        if (label.empty()) {
+          *out_ << "\\(" << vertex.id() << "\\)";
+        } else {
+          *out_ << label;
+        }
+      }
+
+      *out_ << "};" << std::endl;
+
       return out_->good();
     }
 
     bool TikzFormatter::finish_vertices(const Graph &graph) {
-      *out_ << "  \\end{scope}" << std::endl;
+      *out_ << "\\end{scope}" << std::endl;
       return out_->good();
     }
 
     bool TikzFormatter::format_edge(const Edge &edge) {
-      //      const std::string &color = get_color(edge.color());
-      //      *out_ << "  \\draw";
-      //      if (!color.empty()) *out_ << " [" << color << "]";
-      //      *out_ << " (" << edge.from() << ") edge";
-      //      if (!edge.label().empty()) *out_ << " node [auto] {" << edge.label() << "}";
-      //      if (edge.weight() > 0.0) *out_ << " node [swap] {\\(" << edge.weight() << "\\)}";
-      //      *out_ << " (" << edge.to() << ");" << std::endl;
+      *out_ << "\\draw";
+
+      if (edge_colors()) {
+        const ColorType &color = get_color(edge.color());
+        if (!color.empty()) *out_ << " [" << color << "]";
+      }
+
+      *out_ << " (" << edge.from() << ") edge";
+
+      if (edge_labels()) {
+        const Label &label = edge.label();
+        if (!label.empty()) *out_ << " node [auto] {" << edge.label() << "}";
+      }
+
+      if (edge_weights()) *out_ << " node [auto,swap] {\\(" << edge.weight() << "\\)}";
+
+      *out_ << " (" << edge.to() << ");" << std::endl;
+
       return out_->good();
     }
 
--- graph/libgraphutil/tikz_formatter_test.cc
+++ graph/libgraphutil/tikz_formatter_test.cc
@@ -8,8 +8,13 @@
 
 using namespace cavcom::graph;
 
-static const VertexValuesList VERTICES = {{"v1"}, {"v2"}};
-static const EdgeValuesList EDGES = {{0, 1, "e1"}};
+static const VertexValuesList VERTICES = {{"\\(v_1\\)", NOCOLOR, 0, 0}, {Label(), 1, 4, 0}, {"\\(v_3\\)", 2, 1, 3}};
+static const EdgeValuesList EDGES = {{0, 1, "\\(e_1\\)", 0, 1.5}, {1, 2, Label(), 3, 0.0}};
+
+static const TikzFormatter::ColorType C1 = "green!25!white";
+static const TikzFormatter::ColorType C2 = "blue!25!white";
+static const TikzFormatter::ColorType C3 = "red";
+static const TikzFormatter::ColorValuesList COLORS = {{1, C1}, {2, C2}, {3, C3}};
 
 TEST(format_unlabeled_graph) {
   Graph graph(VERTICES, EDGES);
@@ -18,8 +23,98 @@ TEST(format_unlabeled_graph) {
   UNITTEST_ASSERT_TRUE(formatter.format(graph));
   UNITTEST_ASSERT_EQUAL(out.str(),
                         "\\begin{tikzpicture}\n"
-                        "  \\begin{scope}[unlabeled node/.style={draw,circle,fill=#1,inner sep=0cm,"
-                        "minimum size=0.2cm}, unlabeled node/.default={black}]\n"
-                        "  \\end{scope}\n"
+                        "\\begin{scope}[every node/.style={draw,circle,fill=#1,inner sep=0cm,"
+                        "minimum size=0.2cm}, every node/.default={black}]\n"
+                        "\\node (0) at (0,0) {};\n"
+                        "\\node (1) at (4,0) {};\n"
+                        "\\node (2) at (1,3) {};\n"
+                        "\\end{scope}\n"
+                        "\\draw (0) edge (1);\n"
+                        "\\draw (1) edge (2);\n"
+                        "\\end{tikzpicture}\n");
+}
+
+TEST(format_labeled_graph) {
+  Graph graph(VERTICES, EDGES);
+  std::ostringstream out;
+  TikzFormatter formatter(&out);
+  formatter.vertex_labels(true);
+  UNITTEST_ASSERT_TRUE(formatter.format(graph));
+
+  UNITTEST_ASSERT_EQUAL(out.str(),
+                        "\\begin{tikzpicture}\n"
+                        "\\begin{scope}[every node/.style={draw,circle,minimum size=2em}]\n"
+                        "\\node (0) at (0,0) {\\(v_1\\)};\n"
+                        "\\node (1) at (4,0) {\\(1\\)};\n"
+                        "\\node (2) at (1,3) {\\(v_3\\)};\n"
+                        "\\end{scope}\n"
+                        "\\draw (0) edge (1);\n"
+                        "\\draw (1) edge (2);\n"
+                        "\\end{tikzpicture}\n");
+}
+
+TEST(format_unlabeled_vertex_colors) {
+  Graph graph(VERTICES, EDGES);
+  std::ostringstream out;
+  TikzFormatter formatter(&out);
+  formatter.add_colors(COLORS);
+  formatter.vertex_colors(true);
+  UNITTEST_ASSERT_TRUE(formatter.format(graph));
+
+  UNITTEST_ASSERT_EQUAL(out.str(),
+                        "\\begin{tikzpicture}\n"
+                        "\\begin{scope}[every node/.style={draw,circle,fill=#1,inner sep=0cm,"
+                        "minimum size=0.2cm}, every node/.default={black}]\n"
+                        "\\node (0) at (0,0) {};\n"
+                        "\\node [fill=green!25!white] (1) at (4,0) {};\n"
+                        "\\node [fill=blue!25!white] (2) at (1,3) {};\n"
+                        "\\end{scope}\n"
+                        "\\draw (0) edge (1);\n"
+                        "\\draw (1) edge (2);\n"
+                        "\\end{tikzpicture}\n");
+}
+
+TEST(format_labeled_vertex_colors) {
+  Graph graph(VERTICES, EDGES);
+  std::ostringstream out;
+  TikzFormatter formatter(&out);
+  formatter.add_colors(COLORS);
+  formatter.vertex_labels(true);
+  formatter.vertex_colors(true);
+  UNITTEST_ASSERT_TRUE(formatter.format(graph));
+
+  UNITTEST_ASSERT_EQUAL(out.str(),
+                        "\\begin{tikzpicture}\n"
+                        "\\begin{scope}[every node/.style={draw,circle,minimum size=2em}]\n"
+                        "\\node (0) at (0,0) {\\(v_1\\)};\n"
+                        "\\node [fill=green!25!white] (1) at (4,0) {\\(1\\)};\n"
+                        "\\node [fill=blue!25!white] (2) at (1,3) {\\(v_3\\)};\n"
+                        "\\end{scope}\n"
+                        "\\draw (0) edge (1);\n"
+                        "\\draw (1) edge (2);\n"
+                        "\\end{tikzpicture}\n");
+}
+
+TEST(format_edges) {
+  Graph graph(VERTICES, EDGES);
+  std::ostringstream out;
+  TikzFormatter formatter(&out);
+  formatter.add_colors(COLORS);
+  formatter.vertex_labels(true);
+  formatter.vertex_colors(true);
+  formatter.edge_labels(true);
+  formatter.edge_colors(true);
+  formatter.edge_weights(true);
+  UNITTEST_ASSERT_TRUE(formatter.format(graph));
+
+  UNITTEST_ASSERT_EQUAL(out.str(),
+                        "\\begin{tikzpicture}\n"
+                        "\\begin{scope}[every node/.style={draw,circle,minimum size=2em}]\n"
+                        "\\node (0) at (0,0) {\\(v_1\\)};\n"
+                        "\\node [fill=green!25!white] (1) at (4,0) {\\(1\\)};\n"
+                        "\\node [fill=blue!25!white] (2) at (1,3) {\\(v_3\\)};\n"
+                        "\\end{scope}\n"
+                        "\\draw (0) edge node [auto] {\\(e_1\\)} node [auto,swap] {\\(1.5\\)} (1);\n"
+                        "\\draw [red] (1) edge node [auto,swap] {\\(0\\)} (2);\n"
                         "\\end{tikzpicture}\n");
 }
